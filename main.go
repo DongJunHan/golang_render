fd989eecfd52b288d1f2edba52c2cd91fde922a8
DongJunHan <gamedokdok@naver.com>
2021-03-11 22:51:25 +0900

    render 기초 틀

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import(
 	"net/http"
 	"encoding/json"
 	"time"
+	"html/template"
 	"github.com/gorilla/pat"
 )
 
@@ -15,14 +16,38 @@ type User struct{
 }
 
 func getUserInfoHandler(w http.ResponseWriter, r *http.Request){
+	user := User{Name : "dongjun", Email : "dongjun@naver.com"}
 
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))
 }
 
-func addUserHandler(w http.ResponseWriter, r *httpRequest){
+func addUserHandler(w http.ResponseWriter, r *http.Request){
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w,err)
+		return
+	}
+	user.CreatedAt = time.Now()
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))
 
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request){
+	tmpl, err := template.New("Hello").ParseFiles("templates/hello.tmpl")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w,err)
+		return
+	}
+	tmpl.ExecuteTemplate(w, "hello.tmpl", "DongJun")
 
 }
 
